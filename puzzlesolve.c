d70f3c167947c4c507d5cea10f39f6ee73b3da6d
Lucas Handekyn <lucas.handekyn@gmail.com>
2023-11-27 09:05:27 +0100

    possible movements and actions

--- puzzlesolve.c
+++ puzzlesolve.c
@@ -46,8 +46,71 @@ bool isGoalState(const PuzzleState *state)
     return areStatesEqual(state, &goalState);
 }
 
-void performAction(const PuzzleState *currentState, int action, PuzzleState *resultState)
+void performAction(const PuzzleState *currentState, const int *action[][], PuzzleState *resultState)
 {
+    int possibleMovements[4][2] = possibleActions(currentState);
+    int i_0, j_0;
+    for (int i = 0; i < 4; i++)
+    {
+        int new_i = i_0 + possibleMovements[i][0];
+        int new_j = j_0 + possibleMovements[i][1];
+        if (new_i >= 0 && new_i < 3 && new_j >= 0 && new_j < 3)
+        {
+            resultState->board[new_i][new_j] = currentState->board[i_0][j_0];
+            resultState->board[i_0][j_0] = currentState->board[new_i][new_j];
+        }
+    }
+}
+
+int **possibleActions(const PuzzleState *state)
+{
+    // know where 0 is
+    int i_0, j_0;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (state->board[i][j] == 0)
+            {
+                i_0 = i;
+                j_0 = j;
+            }
+        }
+    }
+
+    // Define possible movements: up, down, left, right
+    int movements[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    int possibleMovements[4][2];
+    int count = 0;
+
+    // Check each possible movement
+    for (int i = 0; i < 4; i++)
+    {
+        int new_i = i_0 + movements[i][0];
+        int new_j = j_0 + movements[i][1];
+
+        // If the new position is within the bounds of the board, add it to possibleMovements
+        if (new_i >= 0 && new_i < 3 && new_j >= 0 && new_j < 3)
+        {
+            possibleMovements[count][0] = movements[i][0];
+            possibleMovements[count][1] = movements[i][1];
+            count++;
+        }
+    }
+    return possibleMovements;
+}
+void possibleActions(const PuzzleState *state)
+{
+    // know where 0 is
+    int i_0, j_0;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; i < 3; j++)
+        {
+            if (state->board[i][j] == 0)
+                i_0, j_0 = i, j;
+        }
+    }
 }
 
 void breadthFirstSearch(const PuzzleState *initialState)
